6c0f997e2d314ae007fcf6e981df9fe6ae7ebc80
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2022-01-10 17:39:08 -0300

    chore: create pages

---
+++ src/pages/ViewComicDetails/index.tsx
@@ -0,0 +1,69 @@
+import { Button } from 'components/Button';
+import { Header } from 'components/Header';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from 'services/marvelApi';
+import './style.css';
+
+interface IComic {
+  id: number;
+  digitalId: number;
+  title: string;
+  issueNumber: number;
+  variantDescription: string;
+  description: string;
+  modified: string;
+  isbn: string;
+  upc: string;
+  diamondCode: string;
+  ean: string;
+  issn: string;
+  format: string;
+  pageCount: number;
+  textObjects: Array<string>;
+  resourceURI: string;
+  thumbnail: { path: string; extension: string };
+}
+interface IRequestGetComic {
+  data: { results: Array<IComic> };
+}
+
+function ViewComicDetails() {
+  const { id } = useParams<{ id: string }>();
+  const [comic, setComic] = useState<IComic>();
+  useEffect(() => {
+    async function getComic() {
+      const request = await api.get<IRequestGetComic>(`/comics/${id}`);
+      if (request.data.data.results[0]) {
+        setComic(request.data.data.results[0]);
+      }
+    }
+    getComic();
+  }, [id]);
+  return (
+    <div className="container">
+      <Header title={comic?.title || ''} />
+      <div className="content">
+        <div className="flex image container">
+          <img
+            src={`${comic?.thumbnail.path}.${comic?.thumbnail.extension}`}
+            alt={`Capa do quadrinho ${comic?.title}`}
+          />
+        </div>
+        <div className="flex details">
+          <h3>Número de páginas: {comic?.pageCount} </h3>
+          <h3>
+            Data de publicação:{' '}
+            {comic?.modified && new Date(comic?.modified).toDateString()}
+          </h3>
+        </div>
+        <div className="buttons">
+          <Button name="buy" value="Comprar" />
+          <Button name="back" value="Voltar" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ViewComicDetails;
---
+++ src/pages/ViewComics/index.tsx
@@ -0,0 +1,38 @@
+import { ComicCard } from 'components/ComicCard';
+import { Header } from 'components/Header';
+import './style.css';
+
+function ViewComics() {
+  return (
+    <div className="container">
+      <Header title="Comics" />
+      <div className="content">
+        <input type="text" id="find" placeholder="Search" />
+        <div className="flex cards">
+          <ComicCard
+            name="teste"
+            srcImage="http://i.annihil.us/u/prod/marvel/i/mg/c/80/5e3d7536c8ada.jpg"
+            id="98402"
+          />
+          <ComicCard
+            name="teste"
+            srcImage="http://i.annihil.us/u/prodada.jpg"
+            id="1"
+          />
+          <ComicCard
+            name="teste"
+            srcImage="http://i.annihil.us/u/prod/marvel/i/mg/c/80/5e3d7536c8ada.jpg"
+            id="1"
+          />
+          <ComicCard
+            name="teste"
+            srcImage="http://i.annihil.us/u/prod/marvel/i/mg/c/80/5e3d7536c8ada.jpg"
+            id="1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ViewComics;
